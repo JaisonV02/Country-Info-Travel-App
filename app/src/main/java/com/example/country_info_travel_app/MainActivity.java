0ba11619db5597cbd1cda8590ad8a5952cf99262
Jaison <91572221+JaisonV02@users.noreply.github.com>
2023-12-01 19:01:09 +0000

    Added fragments so screens can be switched when navigation bar icon is selected.

--- app/src/main/java/com/example/country_info_travel_app/MainActivity.java
+++ app/src/main/java/com/example/country_info_travel_app/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.country_info_travel_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.country_info_travel_app.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new ListFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.countries) {
+                replaceFragment(new CountriesFragment());
+            } else if (itemId == R.id.country_lists) {
+                replaceFragment(new ListFragment());
+            } else if (itemId == R.id.flights) {
+                replaceFragment(new FlightFragment());
+            } else if (itemId == R.id.settings) {
+                replaceFragment(new SettingFragment());
+            }
+
+            return true;
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
