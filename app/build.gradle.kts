673922787548d41dc85f87a05b24ca8cea20c3cb
Jaison <91572221+JaisonV02@users.noreply.github.com>
2023-12-15 20:56:17 +0000

    Implemented ROOM database in build.gradle.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,6 @@ dependencies {
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
     implementation("androidx.room:room-runtime:2.6.1")
     annotationProcessor("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.6.2")
+    annotationProcessor("androidx.lifecycle:lifecycle-compiler:2.6.2")
 }
